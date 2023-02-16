905b2a9bb17c6125f331420c74beb38d2f0e4871
unknown <prokop.n@gmail.com>
2023-02-17 00:38:23 +0100

    add Error handling and loading state

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,50 @@ import ConverterForm from "./components/ConverterForm";
 import ConverterResult from "./components/ConverterResult";
 import { useEffect, useState } from "react";
 
-//Add error handling
-//General type for currency list
-//Loading state
-
 function App() {
   const [currencies, setCurrencies] = useState<
     { shortcut: string; name: string }[]
   >([]);
+  const [isErr, setIsErr] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     async function getListOfCurrencies() {
-      const response = await fetch("http://localhost:3000/");
-      const data = await response.json();
-      setCurrencies(data);
+      try {
+        const response = await fetch("http://localhost:3000/");
+
+        if (!response.ok) {
+          throw new Error(
+            "Could not fetch data (status code: " + response.status + ")"
+          );
+        } else {
+          const data = await response.json();
+
+          if (data.error) {
+            setIsErr(data.error);
+          } else {
+            setCurrencies(data.data);
+          }
+          setIsLoading(false);
+        }
+      } catch (err: any) {
+        setIsLoading(false);
+        setIsErr(err.message);
+      }
     }
+    setIsLoading(true);
     getListOfCurrencies();
   }, []);
+
+  console.log(isLoading);
 
   return (
     <div className="content">
       <Card>
-        <ConverterForm currencies={currencies} />
+        {isLoading && <p>Converter is getting ready, please wait.</p>}
+        {!isLoading && <ConverterForm currencies={currencies} />}
         <ConverterResult />
+        {isErr && <p>{isErr}</p>}
       </Card>
     </div>
   );
